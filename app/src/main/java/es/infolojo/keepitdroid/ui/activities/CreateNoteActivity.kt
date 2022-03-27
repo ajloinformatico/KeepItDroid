45d20037c2d527e89cdef50b85ae2e73afcf448c
antoniojoselojoojeda <ajloinformatico@gmail.com>
2022-03-27 12:55:55 +0200

    Edit note fix bug, app base code finished

--- app/src/main/java/es/infolojo/keepitdroid/ui/activities/CreateNoteActivity.kt
+++ app/src/main/java/es/infolojo/keepitdroid/ui/activities/CreateNoteActivity.kt
@@ -1,29 +1,21 @@
 package es.infolojo.keepitdroid.ui.activities
 
-import android.content.Intent
-import android.graphics.drawable.ColorDrawable
-import android.graphics.drawable.Drawable
-import android.net.wifi.hotspot2.pps.Credential
 import android.os.Bundle
-import android.text.Editable
 import android.view.Menu
 import android.view.MenuItem
-import android.widget.EditText
 import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.ui.graphics.Color
-import androidx.core.text.toSpannable
 import com.google.android.material.snackbar.Snackbar
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.firestore.DocumentReference
 import com.google.firebase.firestore.FirebaseFirestore
 import es.infolojo.keepitdroid.R
 import es.infolojo.keepitdroid.data.FireBaseColors
 import es.infolojo.keepitdroid.data.FireBaseModel
 import es.infolojo.keepitdroid.data.vo.SAVE_NOTE_STATE_VO
 import es.infolojo.keepitdroid.databinding.ActivityCreateNoteBinding
-import es.infolojo.keepitdroid.ui.adapters.FireBaseSingleValueEvent
 import es.infolojo.keepitdroid.ui.viewmodels.NotesViewModel
 import es.infolojo.keepitdroid.utils.*
 import timber.log.Timber
@@ -168,35 +160,12 @@ class CreateNoteActivity : AppCompatActivity() {
               NOTE_BODY to noteBody,
               NOTE_COLOR to viewModel.getNoteColor(),
             )
-
             //Create new note
             if (needToUpdateNote.not()) {
-              documentReference.set(dataToSave).addOnSuccessListener {
-                Timber.d(SAVE_NOTE_STATE_VO.NOTE_SAVE.data)
-                showMessage(this, SAVE_NOTE_STATE_VO.NOTE_SAVE.data, Toast.LENGTH_LONG)
-                backToNotesActivity(this)
-
-              }.addOnFailureListener { exception ->
-                Timber.d("Error: $exception")
-                showLastOnCreateNoteError()
-              }
-
+              createNewNote(documentReference, dataToSave, SAVE_NOTE_STATE_VO.NOTE_SAVE.data, )
             // Edit actual note
             } else {
-              documentReference.set(dataToSave).addOnSuccessListener {
-                Timber.d(SAVE_NOTE_STATE_VO.NOTE_SAVE.data)
-                showMessage(this, NOTE_EDIT_SUCCESS, Toast.LENGTH_LONG)
-                backToNotesActivity(this)
-
-              }.addOnFailureListener { exception ->
-                Timber.d("Error: $exception")
-                showLastOnCreateNoteError()
-              }
-
-//              correct child reference.
-//              myRef.setValue("Hello, World")
-//              myRef.child("someChild").child("name").setValue(name)
-
+              deleteNote(title, dataToSave)
             }
 
           } else {
@@ -235,8 +204,20 @@ class CreateNoteActivity : AppCompatActivity() {
     }
   }
 
-  private fun createNewNote() {
+  private fun createNewNote(
+    documentReference: DocumentReference,
+    dataToSave: Map<String, Any>,
+    message: String,
+  ) {
+    documentReference.set(dataToSave).addOnSuccessListener {
+      Timber.d(message)
+      showMessage(this, message, Toast.LENGTH_LONG)
+      backToNotesActivity(this)
 
+    }.addOnFailureListener { exception ->
+      Timber.d("Error: $exception")
+      showLastOnCreateNoteError()
+    }
   }
 
   private fun getIntentContent() {
@@ -263,6 +244,31 @@ class CreateNoteActivity : AppCompatActivity() {
       false
     }
 
+  private fun deleteNote(
+    noteTitle: String,
+    dataToSave: Map<String, Any>
+  ) {
+    if (id.isNullOrEmpty().not()) {
+      fireBaseStore?.let { fStore ->
+        firebaseUser?.let { fUser ->
+          val documentReference: DocumentReference = fStore.collection(
+            FIRE_BASE_COLLECTION_NAME)
+            .document(fUser.uid)
+            .collection(FIRE_BASE_USER_COLLECTION)
+            .document(id.orEmpty())
+
+          documentReference.delete().addOnSuccessListener {
+            Timber.d("$noteTitle has been deleted")
+            createNewNote(documentReference, dataToSave, NOTE_EDIT_SUCCESS)
+
+          }.addOnFailureListener {
+            showError(this, binding.root, "Something was wrong, please try again", Snackbar.LENGTH_LONG)
+            Timber.d("Something was wrong, please try again")
+          }
+        }
+      }
+    }
+  }
 
   private fun hideKeyBoard() {
     hideVirtualKeyBoard(this, binding.titleNote)
